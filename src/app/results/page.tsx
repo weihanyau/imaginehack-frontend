52d6461157500373841d91dc918827ab3176a2ad
Wei Han Yau <weihanyau@gmail.com>
2023-05-21 02:14:23 +0800

    Done results page

---
+++ src/app/results/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import Divider from "@mui/material/Divider";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import { PillButtonFunctional } from "@/components/PillButtonFunctional";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const Container = styled("div")({
+  display: "flex",
+  minHeight: "100vh",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const GreyCard = styled(Card)({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  width: "75vw",
+  height: "80vh",
+  backgroundColor: "#020408",
+  padding: "2rem 4rem",
+  boxShadow: "0px 4px 250px 24px #FFFFFF33",
+  overflow: "auto",
+});
+
+const TextBox = styled(TextField)({
+  backgroundColor: "#0D1116",
+  border: "3px solid #383838",
+  borderRadius: "12px",
+  width: "100%",
+
+  input: {
+    color: "#737578",
+  },
+
+  "input::placeholder": {
+    color: "white",
+  },
+
+  textarea: {
+    color: "#737578",
+  },
+
+  "textarea::placeholder": {
+    color: "white",
+  },
+});
+
+const CardTitle = styled("h3")({});
+
+const CardSubTitle = styled("h4")({
+  color: "#656565",
+});
+
+const StyledTableCell = styled(TableCell)({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#232e3c",
+    color: "#656565",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    backgroundColor: "#0D1116",
+    color: "#656565",
+  },
+});
+
+export default function Create() {
+  return (
+    <Container>
+      <GreyCard>
+        <CardTitle>Interview Title</CardTitle>
+        <TextBox placeholder="Shareable Link" />
+        <Divider
+          variant="middle"
+          sx={{ borderColor: "#383838", borderWidth: "2px" }}
+        />
+        <CardSubTitle>Overall</CardSubTitle>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow sx={{ border: "2px solid #383838" }}>
+                <StyledTableCell>Candidate</StyledTableCell>
+                <StyledTableCell align="right">Confidence</StyledTableCell>
+                <StyledTableCell align="right">Personality</StyledTableCell>
+                <StyledTableCell align="right">Attention</StyledTableCell>
+                <StyledTableCell align="right">Summarization</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow sx={{ border: "2px solid #383838" }}>
+                <StyledTableCell component="th" scope="row">
+                  Candidate
+                </StyledTableCell>
+                <StyledTableCell align="right">Confidence</StyledTableCell>
+                <StyledTableCell align="right">Personality</StyledTableCell>
+                <StyledTableCell align="right">Attention</StyledTableCell>
+                <StyledTableCell align="right">Summarization</StyledTableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </GreyCard>
+    </Container>
+  );
+}
